2efb5d458b841e30ba29882960a9c582697df564
TonyCioara <tonyangelo9707@gmail.com>
2018-04-11 11:55:34 -0700

    Added users to campaign route

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -1,5 +1,5 @@
 class CampaignSerializer < ActiveModel::Serializer
-  attributes :id,  :title, :description, :goal, :money_raised, :image_file_url
+  attributes :id,  :title, :description, :goal, :money_raised, :image_file_url, :user
 
   def image_file_url
     object.image_file.url()
