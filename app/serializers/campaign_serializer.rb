8a11ee78202bf8cf9d28f49fdc12416182fc3987
TonyCioara <tonyangelo9707@gmail.com>
2018-04-16 11:47:46 -0700

    Added user profile image to campaign serializer

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -1,5 +1,6 @@
 class CampaignSerializer < ActiveModel::Serializer
-  attributes :id,  :title, :description, :goal, :money_raised, :image_file_url, :user
+  attributes :id,  :title, :description, :goal, :money_raised, :image_file_url
+  belongs_to :user
 
   def image_file_url
     object.image_file.url()
